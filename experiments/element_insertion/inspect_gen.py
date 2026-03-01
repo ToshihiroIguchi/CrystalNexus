39ff0ffbe7b443dcf3875e6d2cd2d81fbdf497de
Toshihiro Iguchi <toshihiro.iguchi.mail@gmail.com>
2026-03-01 17:29:31 +0900

    chore: Remove experimental prototype scripts and directory

--- experiments/element_insertion/inspect_gen.py
+++
@@ -1,7 +0,0 @@
-from pymatgen.analysis.defects.generators import InterstitialGenerator
-import inspect
-
-print("--- InterstitialGenerator ---")
-print("Init signature:", inspect.signature(InterstitialGenerator.__init__))
-print("generate signature:", inspect.signature(InterstitialGenerator.generate))
-print("get_defects signature:", inspect.signature(InterstitialGenerator.get_defects))
--- experiments/element_insertion/inspect_gen2.py
+++
@@ -1,7 +0,0 @@
-from pymatgen.analysis.defects.generators import VoronoiInterstitialGenerator
-import inspect
-
-print("--- VoronoiInterstitialGenerator ---")
-print("Init signature:", inspect.signature(VoronoiInterstitialGenerator.__init__))
-print("generate signature:", inspect.signature(VoronoiInterstitialGenerator.generate))
-print("get_defects signature:", inspect.signature(VoronoiInterstitialGenerator.get_defects))
